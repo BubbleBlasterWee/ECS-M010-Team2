c03d18b9fae7aaa4c8eb46317a6338748a2c814e
T3ddx <112630646+T3ddx@users.noreply.github.com>
2022-11-09 20:32:38 -0500

    Added comments

--- simulation.py
+++ simulation.py
@@ -2,7 +2,8 @@ from exploit import exploitOnly
 from explore import exploreOnly
 from eGreedy import eGreedy
 
-def simulation(t, e):
+def simulation(t, e=10):
+    #setting up variables
     c1_h = 10
     c2_h = 15
     c3_h = 12
@@ -10,21 +11,21 @@ def simulation(t, e):
 
     optimum_happiness = c2_h*days
 
-    #exlore only
+    #expected happiness and regret for exlore only
     explore_expected_happiness = 100*c1_h+100*c2_h+100*c3_h
     explore_expected_regret = optimum_happiness-explore_expected_happiness
 
-    #exploit only
+    #expected happiness and regret for exploit only
     exploit_expected_happiness = c1_h + c2_h + c3_h + 297*c2_h
     exploit_expected_regret = optimum_happiness-exploit_expected_happiness
 
-    #eGreedy
+    #expected happiness and regret for eGreedy
     percent = ((100-e)/100)
     smaller_percent = (1-percent)/3
     eGreedy_expected_happiness = percent*days*c2_h + smaller_percent*days*c1_h + smaller_percent*days*c2_h + smaller_percent*days*c3_h
     eGreedy_expected_regret = optimum_happiness-eGreedy_expected_happiness
 
-    #average total happiness and regret
+    #simulates total happiness by looping through
     explore_total = 0
     exploit_total = 0
     eGreedy_total = 0
@@ -33,6 +34,7 @@ def simulation(t, e):
         exploit_total += exploitOnly()
         eGreedy_total += eGreedy(e)
 
+    #calculates average total happiness and regret
     average_explore = explore_total/t
     average_exploit = exploit_total/t
     average_eGreedy = eGreedy_total/t
@@ -40,7 +42,7 @@ def simulation(t, e):
     average_regret_exploit = optimum_happiness-average_exploit
     average_regret_eGreedy = optimum_happiness-average_eGreedy
 
-    #prints everything
+    #prints everything neatly
     print("Explore Only:")
     print('Expected Happiness: ' + explore_expected_happiness.__str__())
     print('Expected Regret: ' + explore_expected_regret.__str__())
@@ -56,3 +58,6 @@ def simulation(t, e):
     print('Expected Regret: ' + eGreedy_expected_regret.__str__())
     print('Average happiness for ' + t.__str__() + ' trials: ' + average_eGreedy.__str__())
     print('Average regret for ' + t.__str__() + ' trials: ' + average_regret_eGreedy.__str__())
+
+
+simulation(100000, 10)
