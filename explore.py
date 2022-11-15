565f3f643b54b0d4d39c0a2dbe98b53f369f6143
DZW8 <115006822+DZW8@users.noreply.github.com>
2022-11-15 00:01:05 -0500

    Update explore.py

--- explore.py
+++ explore.py
@@ -3,7 +3,7 @@ import random
 
 def exploreOnly():
     happiness = 0
-    for i in range(100):  # loops for all 300 days; only 100 because we add to happiness 3 times in the for loop, and 300/3=100
+    for i in range(100):  # loops for all 300 days; only 100 b/c we add to happiness 3 times in for loop, and 300/3=100
         happiness = happiness + random.normalvariate(10, 8)  # adds C1
         happiness = happiness + random.normalvariate(15, 6)  # adds C2
         happiness = happiness + random.normalvariate(12, 5)  # adds C3
